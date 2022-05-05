d0cf91bfdacf006ee88e1abcc2dbdf7b2d01f20d
Ermiass <Erohailu@gmail.com>
2022-05-05 14:29:35 -0400

    feat(Search): aded a search

--- src/App.js
+++ src/App.js
@@ -6,26 +6,61 @@ import axios from 'axios';
 function App() {
 
   const [restaurants, setRestaurants] = useState([]);
+  const [newSearch, setNewSearch] = useState('')
 
   const getRestaurants = async () => {
     const url = 'https://data.sfgov.org/resource/pyih-qa8i.json'
     const restaurant  = await axios(url);
     setRestaurants(restaurant.data);
+
   };
+  
 
   useEffect(() => {
     getRestaurants();
   }, []);
+  const handleSearch = (e)=> {
+  
+    setNewSearch(e.target.value);
+  };
+  const refreshSearch = ()=> {
+    setNewSearch('');
+  };
+
  
   return (
     <div>
-      <Search/>
-     <Maps/>
-     { console.log(`DATA: ${restaurants}`) }
-     { restaurants.map( ({ business_name }) => {
-       console.log(business_name)
+      <Search  newSearch={newSearch} handleSearch={handleSearch} refreshSearch={refreshSearch}/>
+      <div className='map-con'>
+     <Maps restaurants ={restaurants}  />
+     {/* {console.log(restaurants)} */}
+     { restaurants
+     .filter((value)=>{
+        if(newSearch ===''){
+          return value;
+        }else{
+          
+            if(value.business_postal_code?.includes(newSearch)||
+            value.business_name?.toLowerCase().includes(newSearch.toLowerCase())){
+           
+              return value;
+              }
+            
+          }
+      })
+      .map( (value) => {
+      //  {console.log(value.business_name)}
+      return (
+        <div >
+          <p>{value.business_name}</p>
+          <p>{value.business_postal_code}</p>
+        </div>
+      
+      )
      })
+  
      }
+     </div>
     </div>
   );
 }
