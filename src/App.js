08781ca027ab2956f7fe93f6f750442b90253ca1
jesusquezada <[]>
2022-05-10 15:13:56 -0700

    refactor(App, PoPUpCard, Maps): refactored the code for popup icons

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Maps from './components/Maps';
 import Card from './components/Card';
 import Search from './components/Search';
 import axios from 'axios';
-import PopUpCard from './components/PopUpCard';
 
 function App() {
 
   const [restaurants, setRestaurants] = useState([]);
   const [newSearch, setNewSearch] = useState('')
-  const [isOpen, setIsOpen] = useState(false);
  
 
   const getRestaurants = async () => {
@@ -32,28 +30,8 @@ function App() {
     setNewSearch('');
   };
 
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  }
-
   return (
     <div>
-    <div>
-    <input
-      type="button"
-      value="Click to Open Popup"
-      onClick={togglePopup}
-    />
-    {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
-    {isOpen && <PopUpCard
-      content={<>
-        <b>Design your Popup</b>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Test button</button>
-      </>}
-      handleClose={togglePopup}
-    />}
-  </div>
     <Header className="header" /> 
     <Search
         newSearch={newSearch} 
