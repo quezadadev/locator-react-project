0647a0b43563070e76f266d478f65247d5a0f3ed
jesusquezada <[]>
2022-05-12 14:59:15 -0700

    style(PopUpCard): changed styles for PopUp feature

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -63,7 +63,9 @@ const Maps = ({ restaurants, newSearch }) => {
          }
       
                         handleClose={togglePopup}
-                      /> </Link>}
+                      /> </Link>
+                      
+                      }
                     </Overlay>
             } 
              
@@ -97,7 +99,9 @@ const Maps = ({ restaurants, newSearch }) => {
                         content={<>
         <b>Design your Popup</b>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Test button</button>
+        <button onClick={ () => {
+                        togglePopup(business_id)}
+                        }>Test button</button>
       </>
          }
       
